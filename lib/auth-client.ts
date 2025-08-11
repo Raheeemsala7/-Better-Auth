dba49372c06e5a7182386ece4e2e3b848c35d522
Raheeemsala7 <rahemsalah534@gmail.com>
2025-08-11 17:43:40 +0300

    add secrt env

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -4,7 +4,7 @@ import { auth } from "@/lib/auth"
 import { ac, roles } from "./premisions"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    // baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+    baseURL: process.env.BETTER_AUTH_URL,
     plugins : [inferAdditionalFields<typeof auth>() , adminClient({ac , roles})]
 
 })
--- lib/auth.ts
+++ lib/auth.ts
@@ -8,6 +8,8 @@ import { ac, roles } from "./premisions";
 
 
 export const auth = betterAuth({
+  secret: process.env.BETTER_AUTH_SECRET!,
+  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
   database: prismaAdapter(prisma, {
     provider: "postgresql"
   }),
